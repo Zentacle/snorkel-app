52a46bea1cf78bcd9c024749526b423f43cfd8ea
Nnanyielugo <kz.onyegbule@gmail.com>
2022-01-24 14:00:03 +0100

    search component with filter icon

--- src/Screens/Search/index.tsx
+++ src/Screens/Search/index.tsx
@@ -1,24 +1,50 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  Dimensions,
+  Platform,
+} from 'react-native';
+
+import SearchInput from '_components/ui/SearchInput';
+const WIDTH = Dimensions.get('window').width;
 
 const Search = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Search</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.contentContainer}>
+        <View style={styles.headerContainer}>
+          <Text style={styles.headerText}>Search</Text>
+        </View>
+        <SearchInput withFilterIcon />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#EFF6F9',
   },
-  text: {
+  contentContainer: {
+    marginBottom: Platform.OS === 'android' ? 100 : 65,
+    paddingBottom: 50,
+  },
+  headerText: {
     color: 'black',
-    fontSize: 18,
+    fontSize: 32,
+    fontWeight: '700',
+    marginHorizontal: 25,
+    marginTop: 30,
+  },
+  headerContainer: {
+    borderBottomColor: 'grey',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    paddingBottom: 20,
   },
 });
 
