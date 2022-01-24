4bc05d26564a6d9dfa44cb2ad265b5f1fabdbb07
Nnanyielugo <kz.onyegbule@gmail.com>
2022-01-24 13:54:22 +0100

    add back button in DiveLogs form for navigation in iOS

--- src/Screens/DiveLogsForms/index.tsx
+++ src/Screens/DiveLogsForms/index.tsx
@@ -1,24 +1,68 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-const DiveLogsForms = () => {
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import type { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import type { CompositeNavigationProp } from '@react-navigation/native';
+import type { FunctionComponent } from 'react';
+import type {
+  RootStackParamList,
+  AppTabsParamList,
+  LogsFormStackParamList,
+} from '_utils/interfaces';
+
+type DiveLogsFormsNavigationProps = CompositeNavigationProp<
+  CompositeNavigationProp<
+    NativeStackNavigationProp<LogsFormStackParamList, 'SimpleLogsForm'>,
+    BottomTabNavigationProp<AppTabsParamList>
+  >,
+  NativeStackNavigationProp<RootStackParamList>
+>;
+
+interface DiveLogsFormsProps {
+  navigation: DiveLogsFormsNavigationProps;
+}
+
+const DiveLogsForms: FunctionComponent<DiveLogsFormsProps> = props => {
+  const goBack = () => {
+    props.navigation.goBack();
+    // props.navigation.navigate('App', {
+    //   screen: 'ExploreStack',
+    //   params: {
+    //     screen: 'Explore',
+    //   },
+    // });
+  };
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Dive Logs Forms</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headerContainer}>
+        <View />
+        <Text style={styles.header}>Create Dive Log</Text>
+        <Icon onPress={goBack} name="close-outline" color="black" size={30} />
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#EFF6F9',
   },
-  text: {
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 10,
+    marginTop: 25,
+  },
+  header: {
     color: 'black',
-    fontSize: 18,
+    fontSize: 22,
+    fontWeight: '700',
+    textAlign: 'center',
+    alignSelf: 'center',
   },
 });
 
