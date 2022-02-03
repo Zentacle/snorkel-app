6960bb8bb574c8544ce63176de20724f1045c62e
Nnanyielugo <kz.onyegbule@gmail.com>
2022-02-03 13:25:28 +0100

    name sub form

--- src/Screens/DiveLogsForms/forms/simple/Name.tsx
+++ src/Screens/DiveLogsForms/forms/simple/Name.tsx
@@ -1,23 +1,60 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import Input from '_components/ui/Input';
+
 const Name = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Name</Text>
+      <View style={styles.nameContentLabel}>
+        <Text style={styles.headerLabel}>Enter Name</Text>
+        <View style={styles.optionalContainer}>
+          <Text style={styles.optionaltext}>Up to 40 Characters</Text>
+        </View>
+      </View>
+      <View>
+        <Input
+          placeholder="Write Title"
+          style={styles.input}
+          containerStyle={styles.inputContainer}
+        />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#2c3e50',
+    marginTop: 30,
+    marginHorizontal: 25,
   },
-  text: {
+  headerLabel: {
     color: 'black',
+    fontSize: 18,
+    fontWeight: '500',
+  },
+  optionalContainer: {},
+  optionaltext: {
+    color: '#aa00ff',
+    fontSize: 12,
+  },
+  nameContentLabel: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    marginVertical: 10,
+    marginHorizontal: 0,
+    paddingHorizontal: 10,
+    borderColor: 'whitesmoke',
+    borderWidth: StyleSheet.hairlineWidth,
+    backgroundColor: '#FFF',
+    borderRadius: 8,
+  },
+  input: {
+    height: 45,
+    fontSize: 16,
   },
 });
 
