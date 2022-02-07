36ca279369850b2f9d6269abdfd7fe0e56f1fed5
Nnanyielugo <kz.onyegbule@gmail.com>
2022-02-07 23:03:36 +0100

    revamp date time depth form

--- src/Screens/DiveLogsForms/forms/advanced/DateTimeDepth.tsx
+++ src/Screens/DiveLogsForms/forms/advanced/DateTimeDepth.tsx
@@ -1,42 +1,49 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import DatePicker from 'react-native-date-picker';
+import { View, StyleSheet } from 'react-native';
+import { Field } from 'react-final-form';
 
+import DateTime from '_components/ui/DateTime';
 import SliderComp from '_components/ui/Slider';
+import { DateTimeMode } from '_screens/DiveLogsForms/utils/interfaces';
 
 const DateTimeDepth = () => {
-  const [date, setDate] = React.useState(new Date());
-
-  const onChange = (selectedDate: Date | undefined) => {
-    const currentDate = selectedDate || date;
-    setDate(currentDate);
-  };
-
   return (
     <View style={styles.container}>
-      <View style={styles.dateContainer}>
-        <Text style={styles.labelText}>Date</Text>
-        <DatePicker date={date} mode="date" onDateChange={onChange} />
-      </View>
-      <View style={styles.timeContainer}>
-        <Text style={styles.labelText}>Start Time</Text>
-        <DatePicker date={date} mode="time" onDateChange={onChange} />
-      </View>
+      <Field
+        name="startDate"
+        component={DateTime}
+        label="Date"
+        mode={DateTimeMode.date}
+      />
+      <Field
+        name="startTime"
+        component={DateTime}
+        label="Time"
+        mode={DateTimeMode.time}
+      />
 
       <View style={{ marginTop: 30 }}>
-        <View style={styles.labelTextContainer}>
-          <Text style={styles.labelText}>Time in Water. Min</Text>
-          <Text style={styles.labelText}>45 min</Text>
-        </View>
-        <SliderComp />
+        <Field
+          name="timeInWater"
+          label="Time in Water . Min"
+          component={SliderComp}
+          trackMarks={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120]}
+          benchMarks={[0, 60, 120]}
+          minimumValue={0}
+          maximumValue={120}
+        />
       </View>
 
       <View style={{ marginTop: 30, marginBottom: 20 }}>
-        <View style={styles.labelTextContainer}>
-          <Text style={styles.labelText}>Max Depth. ft</Text>
-          <Text style={styles.labelText}>40</Text>
-        </View>
-        <SliderComp />
+        <Field
+          name="maxDepth"
+          label="Max Depth. Ft"
+          component={SliderComp}
+          trackMarks={[0, 20, 40, 60, 80, 100, 120, 140, 160]}
+          benchMarks={[0, 80, 160]}
+          minimumValue={0}
+          maximumValue={160}
+        />
       </View>
     </View>
   );
@@ -46,26 +53,6 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#EFF6F9',
     paddingHorizontal: 25,
-  },
-  dateContainer: {
-    alignItems: 'center',
-    marginTop: 30,
-  },
-  timeContainer: {
-    alignItems: 'center',
-    marginTop: 30,
-  },
-  labelText: {
-    textAlign: 'left',
-    alignSelf: 'flex-start',
-    color: 'black',
-    fontSize: 18,
-    fontWeight: '600',
-  },
-  labelTextContainer: {
-    marginBottom: 15,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
   },
 });
 
