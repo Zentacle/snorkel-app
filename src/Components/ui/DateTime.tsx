935dfc4d8054680da2692488c7ee24cb8ab2e387
Nnanyielugo <kz.onyegbule@gmail.com>
2022-02-07 23:02:14 +0100

    date time comp

---
+++ src/Components/ui/DateTime.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import DatePicker from 'react-native-date-picker';
+
+import type { FunctionComponent } from 'react';
+import type { FieldRenderProps } from 'react-final-form';
+
+import type { DateTimeMode } from '_screens/DiveLogsForms/utils/interfaces';
+
+interface DateTimeProps {
+  label: string;
+  mode: DateTimeMode;
+}
+type FinalFormProps = FieldRenderProps<Date, any>;
+type ComponentProps = DateTimeProps & FinalFormProps;
+
+const DateTime: FunctionComponent<ComponentProps> = ({
+  input: { onChange, value },
+  label,
+  mode,
+}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.labelText}>{label}</Text>
+      <DatePicker
+        date={value || new Date()}
+        mode={mode}
+        onDateChange={(dateValue: Date) => onChange(dateValue)}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    marginTop: 30,
+  },
+  labelText: {
+    textAlign: 'left',
+    alignSelf: 'flex-start',
+    color: 'black',
+    fontSize: 18,
+    fontWeight: '600',
+  },
+});
+
+export default DateTime;
