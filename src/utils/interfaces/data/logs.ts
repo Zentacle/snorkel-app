64258c295defd4a5ef4e5c46f7901c7e3cf0eb28
Nnanyielugo <kz.onyegbule@gmail.com>
2022-02-08 22:38:33 +0100

    remove optional props

--- src/utils/interfaces/data/logs.ts
+++ src/utils/interfaces/data/logs.ts
@@ -6,20 +6,20 @@ export interface SimpleFormInitialValues {
 }
 
 export interface AdvancedFormInitialValues {
-  rating?: number;
-  difficulty?: string;
-  name?: string;
-  note?: string;
-  startDate?: Date;
-  startTime?: Date;
-  timeInWater?: number;
-  maxDepth?: number;
-  waterTemp?: number;
-  airTemp?: number;
-  visibility?: number;
-  diveActivity?: string;
-  entry?: string;
-  weight?: number;
+  rating: number;
+  difficulty: string;
+  name: string;
+  note: string;
+  startDate: Date;
+  startTime: Date;
+  timeInWater: number;
+  maxDepth: number;
+  waterTemp: number;
+  airTemp: number;
+  visibility: number;
+  diveActivity: string;
+  entry: string;
+  weight: number;
   airTankStart: number;
   airTankEnd: number;
   nitrox: string;
