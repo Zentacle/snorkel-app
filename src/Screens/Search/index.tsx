2e176c12d350e6def6da061d85acd55ee90e9a46
Nnanyielugo <kz.onyegbule@gmail.com>
2022-02-16 21:01:11 +0100

    search filter modal

--- src/Screens/Search/index.tsx
+++ src/Screens/Search/index.tsx
@@ -8,18 +8,75 @@ import {
   // Dimensions,
   Platform,
 } from 'react-native';
+import { Form, Field } from 'react-final-form';
 
 import SearchInput from '_components/ui/SearchInput';
+import AutocompleteModal from './components/AutocompleteModal';
+import SearchFiltersModal from './components/SearchFiltersModal';
+
 // const WIDTH = Dimensions.get('window').width;
 
+interface InitialValues {
+  difficulty: string;
+  preference: string;
+  entry: string;
+  maxDepth: number;
+}
+
 const Search = () => {
+  const [autocompleteModalOpen, toggleAutocompleteModal] =
+    React.useState(false);
+  const [filtersModalopen, toggleFiltersModal] = React.useState(false);
+
+  const handleInputFocus = () => {
+    toggleAutocompleteModal(true);
+  };
+
+  const handleToggleFiltersModal = () => {
+    toggleFiltersModal(!filtersModalopen);
+  };
+
+  const initialValues: InitialValues = {
+    difficulty: 'Beginner',
+    preference: 'Scuba',
+    entry: 'Shore',
+    maxDepth: 18,
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.contentContainer}>
         <View style={styles.headerContainer}>
           <Text style={styles.headerText}>Search</Text>
         </View>
-        <SearchInput withFilterIcon />
+        <Form
+          onSubmit={() => {}}
+          initialValues={initialValues}
+          render={({ values, form }) => {
+            return (
+              <View>
+                <Field
+                  name="search"
+                  isVisible={autocompleteModalOpen}
+                  component={AutocompleteModal}
+                  closeModal={() => toggleAutocompleteModal(false)}
+                />
+                <Field
+                  name="search"
+                  component={SearchInput}
+                  withFilterIcon
+                  handleInputFocus={handleInputFocus}
+                  onClickFilterIcon={handleToggleFiltersModal}
+                />
+                <SearchFiltersModal
+                  isVisible={filtersModalopen}
+                  closeModal={handleToggleFiltersModal}
+                  reset={form.reset}
+                />
+              </View>
+            );
+          }}
+        />
       </ScrollView>
     </SafeAreaView>
   );
