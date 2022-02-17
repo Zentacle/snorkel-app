198aa0e66517b6ab44e7038c8b1b5bab916abeeb
Nnanyielugo <kz.onyegbule@gmail.com>
2022-02-17 11:19:57 +0100

    search main view

---
+++ src/Screens/Search/interfaces.ts
@@ -0,0 +1,7 @@
+import type { ImageSourcePropType } from 'react-native';
+
+export interface Destination {
+  place: string;
+  country: string;
+  image: ImageSourcePropType;
+}
---
+++ src/Screens/Search/utils.ts
@@ -0,0 +1,49 @@
+import type { Destination } from './interfaces';
+
+import DiveSite1 from '_assets/DiveSite.jpg';
+import DiveSite2 from '_assets/DiveSite2.jpg';
+import DiveSite3 from '_assets/DiveSite3.jpg';
+import DiveSite4 from '_assets/DiveSite4.jpg';
+
+export const destinations: Destination[] = [
+  {
+    place: 'Bali',
+    country: 'Indonesia',
+    image: DiveSite1,
+  },
+  {
+    place: 'Palazo',
+    country: 'Indonesia',
+    image: DiveSite2,
+  },
+  {
+    place: 'Baohol',
+    country: 'Indonesia',
+    image: DiveSite3,
+  },
+  {
+    place: 'Koh Tao',
+    country: 'Indonesia',
+    image: DiveSite4,
+  },
+  {
+    place: 'Bali',
+    country: 'Indonesia',
+    image: DiveSite1,
+  },
+  {
+    place: 'Palazo',
+    country: 'Indonesia',
+    image: DiveSite2,
+  },
+  {
+    place: 'Baohol',
+    country: 'Indonesia',
+    image: DiveSite3,
+  },
+  {
+    place: 'Koh Tao',
+    country: 'Indonesia',
+    image: DiveSite4,
+  },
+];
