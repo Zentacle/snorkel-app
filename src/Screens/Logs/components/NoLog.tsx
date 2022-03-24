8c3d7ef7d7c493f57805e6d131afb4b4b038e4db
Nnanyielugo <kz.onyegbule@gmail.com>
2022-03-24 14:42:29 +0100

    No dive log from deep linnk screen

---
+++ src/Screens/Logs/components/NoLog.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+  Dimensions,
+  Image,
+} from 'react-native';
+import FEIcon from 'react-native-vector-icons/Feather';
+
+import type { FunctionComponent } from 'react';
+
+import SadEmoji from '_assets/sad-emo.png';
+
+interface NoLogProps {
+  goBack: () => void;
+}
+
+const NoLog: FunctionComponent<NoLogProps> = ({ goBack }) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.contentContainer}>
+        <TouchableWithoutFeedback onPress={goBack}>
+          <FEIcon name="chevron-left" color="black" size={25} />
+        </TouchableWithoutFeedback>
+        <View style={styles.messageContainer}>
+          <Image style={styles.image} source={SadEmoji} />
+          <Text style={styles.message}>
+            Sorry, the Dive Log you are looking for does not exist!
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EFF6F9',
+  },
+  contentContainer: {
+    marginTop: 25,
+    marginHorizontal: 25,
+  },
+  messageContainer: {
+    marginTop: Dimensions.get('window').height * 0.15,
+  },
+  message: {
+    fontWeight: '600',
+    fontSize: 18,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    alignSelf: 'center',
+  },
+});
+
+export default NoLog;
