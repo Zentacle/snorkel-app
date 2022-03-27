9ce404ca7c6f420a2047a45297658517c9b04a62
Nnanyielugo <kz.onyegbule@gmail.com>
2022-03-27 15:52:34 +0100

    put screen size rules into constants

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -1,1 +1,10 @@
-export {};
+import { Dimensions } from 'react-native';
+
+export const WIDTH = Dimensions.get('window').width;
+export const HEIGHT = Dimensions.get('screen').height;
+
+const heightThreshold = 780;
+const widthThreshold = 380;
+
+export const isBelowHeightThreshold = HEIGHT < heightThreshold;
+export const isBelowWidthThreshold = WIDTH < widthThreshold;
