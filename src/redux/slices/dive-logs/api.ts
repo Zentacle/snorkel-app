7aa6c57f6812dd3886e01959d3b0bde8a7d760be
Nnanyielugo <kz.onyegbule@gmail.com>
2022-04-18 23:17:15 +0100

    dive logs api

--- src/redux/slices/dive-logs/api.ts
+++ src/redux/slices/dive-logs/api.ts
@@ -1,21 +1,35 @@
 import config from 'react-native-config';
-
 import {
   SimpleFormInitialValues,
   AdvancedFormInitialValues,
-  DiveLogReturnValues,
+  SimpleDiveLogReturnValues,
+  AdvancedDiveLogReturnValues,
+  DiveLogsState,
 } from '_utils/interfaces/data/logs';
+
+import { makeCookieHeaders } from '_utils/functions';
+interface OwnDiveLogsResponse {
+  data: {
+    reviews: DiveLogsState[];
+  };
+  msg: string;
+}
 
 export async function handleCreateDiveLog(
   body: SimpleFormInitialValues,
-): Promise<DiveLogReturnValues> {
+  auth_cookie: string,
+  auth_token: string,
+): Promise<SimpleDiveLogReturnValues> {
   try {
     const url = `${config.API_ENDPOINT}/review/add`;
+    // const csrf_token = makeCookieHeaders(auth_cookie).csrf_access_token;
     const response = fetch(url, {
       method: 'POST',
       body: JSON.stringify(body),
       headers: {
-        'Content-Type': 'application/json',
+        'content-type': 'application/json',
+        // 'X-CSRF-TOKEN': csrf_token,
+        Authorization: `Bearer ${auth_token}`,
       },
     }).then(res => res.json());
     return response;
@@ -26,12 +40,52 @@ export async function handleCreateDiveLog(
 
 export async function handleUpdateDiveLog(
   body: AdvancedFormInitialValues,
-): Promise<DiveLogReturnValues> {
+  auth_cookie: string,
+): Promise<AdvancedDiveLogReturnValues> {
   try {
     const url = `${config.API_ENDPOINT}/review/patch`;
+    const csrf_token = makeCookieHeaders(auth_cookie).csrf_access_token;
     const response = fetch(url, {
       method: 'PATCH',
       body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-TOKEN': csrf_token,
+      },
+    }).then(res => res.json());
+    return response;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export async function handleFetchOwnDiveLogs(
+  auth_cookie: string,
+  username: string,
+): Promise<OwnDiveLogsResponse> {
+  try {
+    const url = `${config.API_ENDPOINT}/user/get?username=${username}`;
+    const csrf_token = makeCookieHeaders(auth_cookie).csrf_access_token;
+    const response = fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-TOKEN': csrf_token,
+      },
+    }).then(res => res.json());
+    return response;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export async function handleFetchSingleDiveLog(
+  id: number,
+): Promise<AdvancedDiveLogReturnValues> {
+  try {
+    const url = `${config.API_ENDPOINT}/review/get?review_id=${id}`;
+    const response = fetch(url, {
+      method: 'GET',
       headers: {
         'Content-Type': 'application/json',
       },
--- src/redux/slices/dive-logs/index.ts
+++ src/redux/slices/dive-logs/index.ts
@@ -1,40 +1,96 @@
-import { createSlice, createSelector } from '@reduxjs/toolkit';
-import type { PayloadAction } from '@reduxjs/toolkit';
+import {
+  createSlice,
+  createSelector,
+  createAsyncThunk,
+} from '@reduxjs/toolkit';
 
-import { AppThunk, RootState } from '../../store';
-import { AdvancedFormInitialValues as DiveLog } from '_utils/interfaces/data/logs';
+import { RootState } from '../../store';
+import { DiveLogsState } from '_utils/interfaces/data/logs';
+import { handleFetchOwnDiveLogs } from './api';
 
-interface DiveLogsState {
+interface DiveLogsStoreState {
   diveLogs: {
-    [id: number]: DiveLog;
+    [id: number]: DiveLogsState;
+  };
+  loading: boolean;
+  error: {
+    status: boolean;
+    message: string | null;
   };
 }
 
-const initialState: DiveLogsState = {
+interface NormalizedObj {
+  [id: string]: DiveLogsState;
+}
+
+const initialState: DiveLogsStoreState = {
   diveLogs: {},
+  loading: false,
+  error: {
+    status: false,
+    message: null,
+  },
 };
+
+const normalizeData = (data: DiveLogsState[]) => {
+  const normalizedObj: NormalizedObj = {};
+  for (let item of data) {
+    normalizedObj[item.id.toString()] = item;
+  }
+  return normalizedObj;
+};
+
+interface UserAuthCookie {
+  auth_cookie: string;
+  username: string;
+}
+
+export const fetchOwnDiveLogs = createAsyncThunk(
+  'dive-logs/fetch-dive-logs',
+  async (userAuth: UserAuthCookie, thunkApi) => {
+    const response = await handleFetchOwnDiveLogs(
+      userAuth.auth_cookie,
+      userAuth.username,
+    );
+    if (response.msg) {
+      return thunkApi.rejectWithValue(response.msg);
+    }
+    return response.data;
+  },
+);
 
 export const diveLogsSlice = createSlice({
   name: 'dive-logs',
   initialState: initialState,
-  reducers: {
-    getAllDiveLogs: (state, action: PayloadAction<DiveLog[]>) => {
-      state.diveLogs = action.payload;
-    },
-    // NOTE:  this should be temporary and a resolution done as soon as funn api integration is achieved
-    saveDiveLog: (state, action: PayloadAction<DiveLog>) => {
-      // state.diveLogs.push(action.payload);
-      state.diveLogs[action.payload.id as number] = action.payload;
-    },
-    editDiveLog: (state, action: PayloadAction<DiveLog>) => {
-      state.diveLogs[action.payload.id as number] = action.payload;
-    },
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(fetchOwnDiveLogs.pending, state => {
+        state.loading = true;
+        state.error.status = false;
+        state.error.message = null;
+        // state.diveLogs = {};
+      })
+      .addCase(fetchOwnDiveLogs.rejected, (state, action) => {
+        state.loading = false;
+        state.error.status = true;
+        state.error.message =
+          typeof action.payload === 'string'
+            ? action.payload
+            : 'There was an fetching dive logs. Please try again later';
+      })
+      .addCase(fetchOwnDiveLogs.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error.status = false;
+        state.error.message = null;
+        state.diveLogs = normalizeData(action.payload.reviews);
+      });
   },
 });
 
-export const { saveDiveLog, editDiveLog } = diveLogsSlice.actions;
-
 export const selectAllDiveLogs = (state: RootState) => state.dive_logs.diveLogs;
+export const selectDiveLogsLoadingState = (state: RootState) =>
+  state.dive_logs.loading;
 
 export const selectDiveLogById = (id: number) => {
   const selectedDiveLog = createSelector(
