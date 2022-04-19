1c96f41e5801b40137c92152c5c258f653414dd8
Nnanyielugo <kz.onyegbule@gmail.com>
2022-04-19 12:25:42 +0100

    move fetchNearby to api

--- src/redux/slices/dive-sites/api.ts
+++ src/redux/slices/dive-sites/api.ts
@@ -3,10 +3,12 @@ import { Spot } from '_utils/interfaces/data/spot';
 
 interface ResponseWithSpots {
   data: Spot[];
+  msg: string;
 }
 
 interface ResponseWithSpot {
   data: Spot;
+  msg: string;
 }
 
 export async function fetchDiveSites(): Promise<ResponseWithSpots> {
@@ -46,6 +48,21 @@ export async function fetchNearby(
 ): Promise<ResponseWithSpots> {
   try {
     const url = `${config.API_ENDPOINT}/spots/nearby?beach_id=${beach_id}`;
+    const response = fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then(res => res.json());
+    return response;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export async function fetchRecommended(): Promise<ResponseWithSpots> {
+  try {
+    const url = `${config.API_ENDPOINT}/spots/recs`;
     const response = fetch(url, {
       method: 'GET',
       headers: {
