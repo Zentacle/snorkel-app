73c8f39217bf17b92611f39270010e61a6d6dab3
Nnanyielugo <kz.onyegbule@gmail.com>
2022-04-19 12:25:19 +0100

    migrate diveSites thunks to use createAsyncThunks

--- src/redux/slices/dive-sites/index.ts
+++ src/redux/slices/dive-sites/index.ts
@@ -1,9 +1,12 @@
-import { createSlice, createSelector } from '@reduxjs/toolkit';
-import type { PayloadAction } from '@reduxjs/toolkit';
+import {
+  createSlice,
+  createSelector,
+  createAsyncThunk,
+} from '@reduxjs/toolkit';
 
-import { fetchDiveSites, fetchNearby, fetchDiveSite } from './api';
+import { fetchDiveSites, fetchDiveSite, fetchRecommended } from './api';
 import { Spot } from '_utils/interfaces/data/spot';
-import { AppThunk, RootState } from '../../store';
+import { RootState } from '../../store';
 
 interface NormalizedObj {
   [id: string]: Spot;
@@ -24,22 +27,71 @@ const initialState: DiveSpotState = {
   },
 };
 
+export const handleFetchRecommended = createAsyncThunk(
+  'dive-sites/recommended',
+  async (_, thunkApi) => {
+    const response = await fetchRecommended();
+    if (!response.data) {
+      return thunkApi.rejectWithValue(response.msg);
+    }
+    return response.data;
+  },
+);
+
+export const handleFetchDiveSites = createAsyncThunk(
+  'dive-sites/fetchdiveSites',
+  async (_, thunkApi) => {
+    const response = await fetchDiveSites();
+    if (!response.data) {
+      return thunkApi.rejectWithValue(response.msg);
+    }
+    return response.data;
+  },
+);
+
+export const handleFetchDiveSite = createAsyncThunk(
+  'dive-sites/fetchdiveSite',
+  async (id: number, thunkApi) => {
+    const response = await fetchDiveSite(id);
+    if (!response.data) {
+      return thunkApi.rejectWithValue(response.msg);
+    }
+    return response.data;
+  },
+);
+
 export const diveSitesSlice = createSlice({
   name: 'dive-sites',
   initialState: initialState,
-  reducers: {
-    setLoading: (state, action: PayloadAction<boolean>) => {
-      state.loading = action.payload;
-    },
-    getAllDiveSites: (state, action: PayloadAction<Spot[]>) => {
-      state.diveSpots = normalizeData(action.payload);
-    },
-    getDiveSite: (state, action: PayloadAction<Spot>) => {
-      state.diveSpots[action.payload.id] = action.payload;
-    },
-    setError: (state, action: PayloadAction<boolean>) => {
-      state.error.status = action.payload;
-    },
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(handleFetchDiveSites.pending, state => {
+        state.loading = true;
+        state.error.status = false;
+      })
+      .addCase(handleFetchDiveSites.rejected, state => {
+        state.loading = false;
+        state.error.status = true;
+      })
+      .addCase(handleFetchDiveSites.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error.status = false;
+        state.diveSpots = normalizeData(action.payload);
+      })
+      .addCase(handleFetchDiveSite.pending, state => {
+        state.loading = true;
+        state.error.status = false;
+      })
+      .addCase(handleFetchDiveSite.rejected, state => {
+        state.loading = false;
+        state.error.status = true;
+      })
+      .addCase(handleFetchDiveSite.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error.status = false;
+        state.diveSpots[action.payload.id] = action.payload;
+      });
   },
 });
 
@@ -50,9 +102,6 @@ const normalizeData = (data: Spot[]) => {
   }
   return normalizedObj;
 };
-
-const { getAllDiveSites, getDiveSite, setLoading, setError } =
-  diveSitesSlice.actions;
 
 export const selectAllDiveSites = (state: RootState) =>
   state.dive_sites.diveSpots;
@@ -71,53 +120,12 @@ export const selectLoadingState = (state: RootState) =>
 
 export const selectErrorState = (state: RootState) => state.dive_sites.error;
 
-export const handleFetchDiveSites =
-  (): AppThunk => async (dispatch, _getState) => {
-    try {
-      dispatch(setLoading(true));
-      const diveSites = await fetchDiveSites();
-      dispatch(getAllDiveSites(diveSites.data));
-      dispatch(setLoading(false));
-      dispatch(setError(false));
-    } catch (err) {
-      dispatch(setError(true));
-      throw err;
-    }
-  };
-
-export const handleFetchDiveSite =
-  (id: number): AppThunk =>
-  async (dispatch, _getState) => {
-    try {
-      dispatch(setLoading(true));
-      const diveSite = await fetchDiveSite(id);
-      // add divesite to dive sites object in state so we needen't make
-      // network calls if a dive site exists in state
-      dispatch(getDiveSite(diveSite.data));
-      dispatch(setLoading(false));
-      dispatch(setError(false));
-    } catch (err) {
-      dispatch(setError(true));
-      throw err;
-    }
-  };
-
 export const isDiveSiteDetailinState = (id: number) => {
   return createSelector(
     [selectAllDiveSites],
     selectedDiveSites =>
       Boolean(selectedDiveSites[id]) && Boolean(selectedDiveSites[id].ratings),
   );
-};
-
-export const handleFetchNearby = async (id: number) => {
-  try {
-    const recommended = await fetchNearby(id);
-    return recommended.data;
-  } catch (err) {
-    console.log(err);
-    throw err;
-  }
 };
 
 export default diveSitesSlice.reducer;
