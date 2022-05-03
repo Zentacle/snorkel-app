97dc582024ddcda364187101868ad496f5037785
Nnanyielugo <kz.onyegbule@gmail.com>
2022-05-03 09:49:46 +0100

    more adjustment to dive log images carousel

--- src/Components/reusables/DiveLogImageCarousel.tsx
+++ src/Components/reusables/DiveLogImageCarousel.tsx
@@ -221,7 +221,7 @@ const styles = StyleSheet.create({
   header: {},
   headerIconsContainer: {
     position: 'absolute',
-    top: Platform.OS === 'ios' ? 62 : 40,
+    top: Platform.OS === 'ios' ? 67 : 45,
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: '100%',
