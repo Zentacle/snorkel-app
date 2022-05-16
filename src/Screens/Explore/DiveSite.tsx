88fbd835521220f8402dcaf9a15ae1a8ef515d43
Nnanyielugo <kz.onyegbule@gmail.com>
2022-05-16 19:13:00 +0100

    use 'url' property for share

--- src/Screens/Explore/DiveSite.tsx
+++ src/Screens/Explore/DiveSite.tsx
@@ -192,7 +192,7 @@ const DiveSite: FunctionComponent<DiveSiteProps> = ({ navigation, route }) => {
         showsVerticalScrollIndicator={false}>
         <ImageCarousel
           goBack={navigateBack}
-          shareUrl={`https://zentacle.com/Beach/${diveSite.id}/${diveSite.name}`}
+          shareUrl={`https://zentacle.com/${diveSite.url}`}
           images={diveSite.images}
         />
 
