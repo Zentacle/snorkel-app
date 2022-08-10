84441774cfb4afca93c37e5a01048d009d7dc4e4
Nnanyielugo <kz.onyegbule@gmail.com>
2022-08-10 18:27:57 +0100

    fill out onboarding pro upsell screen

--- src/Screens/Onboarding/ProUpsell.tsx
+++ src/Screens/Onboarding/ProUpsell.tsx
@@ -2,15 +2,53 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
 import ProUpsellDisplay from '_components/reusables/pro-upsell/ProUpsellDisplay';
+import { sendEvent } from '_utils/functions/amplitude';
 
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import type { CompositeNavigationProp } from '@react-navigation/native';
 import type { FunctionComponent } from 'react';
+import type {
+  RootStackParamList,
+  OnboardingStackParamList,
+} from '_utils/interfaces';
 
 interface ProUpsellProps {}
 
-const ProUpsell: FunctionComponent<ProUpsellProps> = () => {
+type ProUpsellNavigationProps = CompositeNavigationProp<
+  NativeStackNavigationProp<OnboardingStackParamList, 'ProUpsellDisplay'>,
+  NativeStackNavigationProp<RootStackParamList>
+>;
+
+interface ProUpsellProps {
+  navigation: ProUpsellNavigationProps;
+}
+
+const ProUpsell: FunctionComponent<ProUpsellProps> = ({ navigation }) => {
+  React.useEffect(() => {
+    sendEvent('page_view', {
+      type: 'pro_upsell',
+    });
+  }, []);
+
+  const navigateToWebView = (url: string) => {
+    navigation.navigate('AppWebview', {
+      source: url,
+    });
+  };
+
+  const navigateToApp = () => {
+    navigation.navigate('App', {
+      screen: 'Explore',
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <ProUpsellDisplay />
+      <ProUpsellDisplay
+        closeAction={navigateToApp}
+        navigateToWebView={navigateToWebView}
+        closeText="Skip"
+      />
     </View>
   );
 };
