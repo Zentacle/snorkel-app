9b5b6cc1fd567c003e776041b2dee313cfdc0b3d
Nnanyielugo <kz.onyegbule@gmail.com>
2022-08-23 11:50:25 +0100

    migrate Ratings to new select design and create V2 SelectGradientBorder component for it

--- src/Screens/DiveLogsForms/forms/simple/Rating.tsx
+++ src/Screens/DiveLogsForms/forms/simple/Rating.tsx
@@ -4,13 +4,12 @@ import { Field } from 'react-final-form';
 import { useTranslation } from 'react-i18next';
 
 import RatingsInputComp from '_components/ui/RatingsInputComp';
-import SelectWGradientBorder from '_components/ui/SelectWGradientBoder';
+import SelectWGradientBorder from '_components/ui/SelectWGradientBorderV2';
 import {
   ActiveComponent,
   InactiveComponent,
 } from '_utils/form/gradient-selection';
 import { capitalize } from '_utils/functions';
-import { WIDTH } from '_utils/constants';
 
 const Rating = () => {
   const { t } = useTranslation();
@@ -32,17 +31,6 @@ const Rating = () => {
         <Field name="rating" component={RatingsInputComp} />
       </View>
 
-      <View style={styles.levelContentContainer}>
-        <Text style={styles.headerLabel}>{t('LEVEL_OF_DIFFICULTY')}</Text>
-        <Field
-          name="difficulty"
-          component={SelectWGradientBorder}
-          activeComponent={ActiveComponent}
-          inactiveComponent={InactiveComponent}
-          options={levels}
-        />
-      </View>
-
       <View style={styles.activityContentContainer}>
         <Text style={styles.headerLabel}>{t('DIVE_ACTIVITY')}</Text>
         <Field
@@ -51,6 +39,17 @@ const Rating = () => {
           options={activity}
           activeComponent={ActiveComponent}
           inactiveComponent={InactiveComponent}
+        />
+      </View>
+
+      <View style={styles.levelContentContainer}>
+        <Text style={styles.headerLabel}>{t('LEVEL_OF_DIFFICULTY')}</Text>
+        <Field
+          name="difficulty"
+          component={SelectWGradientBorder}
+          activeComponent={ActiveComponent}
+          inactiveComponent={InactiveComponent}
+          options={levels}
         />
       </View>
     </ScrollView>
@@ -68,56 +67,12 @@ const styles = StyleSheet.create({
     fontWeight: '500',
   },
   levelContentContainer: {
-    marginVertical: 40,
+    marginTop: 10,
+    marginBottom: 20,
   },
   activityContentContainer: {
     marginTop: 30,
     marginBottom: 20,
-  },
-  activityContainer: {
-    marginTop: 20,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  activity: {
-    backgroundColor: '#fff',
-    borderRadius: 12,
-    opacity: 0.5,
-    width: '30%',
-  },
-  selectedActivity: {
-    backgroundColor: '#fff',
-    borderRadius: 12,
-    paddingVertical: 1.5,
-    paddingHorizontal: 1.5,
-    elevation: 2,
-  },
-  activityText: {
-    marginLeft: 10,
-    marginBottom: 10,
-    color: 'black',
-    fontSize: WIDTH < 380 ? 13 : 14,
-  },
-  normalActivityCircle: {
-    width: 20,
-    height: 20,
-    borderRadius: 10,
-    backgroundColor: '#EFF6F9',
-    marginTop: 15,
-    marginBottom: 15,
-    marginLeft: 10,
-  },
-  selectedActivityCircle: {
-    width: 20,
-    height: 20,
-    borderRadius: 10,
-    backgroundColor: '#EFF6F9',
-    marginTop: 15,
-    marginBottom: 15,
-    marginLeft: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
 
