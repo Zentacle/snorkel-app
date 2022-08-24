c6fb468b0be5a257b3fd912b0eb0f496b1212553
Nnanyielugo <kz.onyegbule@gmail.com>
2022-08-24 13:58:38 +0100

    fix removing item after sync

--- src/utils/functions/offline-manager.ts
+++ src/utils/functions/offline-manager.ts
@@ -1,9 +1,6 @@
 import AsyncStorage from '@react-native-community/async-storage';
 
-export async function saveItem<ObjType>(
-  key: string,
-  item: ObjType,
-): Promise<void> {
+async function saveItem<ObjType>(key: string, item: ObjType): Promise<void> {
   const items = await fetchItems<ObjType>(key);
   items.push(item);
   await AsyncStorage.setItem(key, JSON.stringify(items));
@@ -13,18 +10,18 @@ interface UploadedBoolean {
   isUploaded: boolean;
 }
 
-export async function syncItems<ObjType>(
+async function syncItems<ObjType>(
   key: string,
   fn: (item: ObjType) => Promise<any>,
 ): Promise<void> {
   type CombinedTypes = ObjType & UploadedBoolean;
 
   const items = await fetchItems<CombinedTypes>(key);
-  items.forEach(item => {
-    fn(item).then(() => {
-      item.isUploaded = true;
-    });
-  });
+  await Promise.all(
+    items.map(item => {
+      fn(item).then(() => (item.isUploaded = true));
+    }),
+  );
 
   await AsyncStorage.setItem(
     key,
@@ -32,8 +29,10 @@ export async function syncItems<ObjType>(
   );
 }
 
-export async function fetchItems<ObjType>(key: string): Promise<ObjType[]> {
+async function fetchItems<ObjType>(key: string): Promise<ObjType[]> {
   const itemStr = await AsyncStorage.getItem(key); // assume itemStr is a stringified array of objs
   const items: ObjType[] = itemStr ? JSON.parse(itemStr) : [];
   return items;
 }
+
+export default { saveItem, syncItems, fetchItems };
