15986b25544fe059cc57a1e4cce05d01f98885d0
Nnanyielugo <kz.onyegbule@gmail.com>
2023-01-27 13:33:16 +0100

    fix search result bug and make transition to dive site screen

--- src/Navigation/App/Explore.tsx
+++ src/Navigation/App/Explore.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createSharedElementStackNavigator } from 'react-navigation-shared-element';
 
 import type { ExploreStackParamList } from '_utils/interfaces';
